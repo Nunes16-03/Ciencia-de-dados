dfbfaf762da075102a3bfe74d9c422474b5535b3
Nunes16-03 <viniciusln2020@gmail.com>
2025-08-19 08:30:16 -0300

    terceiro commit

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 nome = (input("Digite seu nome: "))
 print(f"Olá, {nome}! Bem-vindo ao nosso programa.")
+idade = int(input("Digite sua idade: "))
+if idade < 18:
+    print("Você é menor de idade.")
+else:
+    print("Você é maior de idade.")
+print("Obrigado por participar!")
+# Fim do programa
